3cbc7e3f065a00eebb5ab351ec5ead9f5006479f
Saurabh Saha <vksaha99340@gmail.com>
2022-12-28 10:47:46 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyARWTzUgvL4eUVcJgQ8KniDCrjhABKBdv0",
+  authDomain: "clone-67a3a.firebaseapp.com",
+  projectId: "clone-67a3a",
+  storageBucket: "clone-67a3a.appspot.com",
+  messagingSenderId: "489410911678",
+  appId: "1:489410911678:web:cbba299924874910fcadef",
+};
